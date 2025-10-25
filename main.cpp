b40fb81c0041948f349e3d6ee6a0d516f6124ce5
meentallyy <a.galk.2019@yandex.ru>
2025-10-26 00:27:23 +0300

    Изменение файла main.cpp разработчиком №3

--- main.cpp
+++ main.cpp
@@ -25,8 +25,9 @@ int main() {
         cout << "Файл input.txt не найден. Используются значения по умолчанию." << endl;
     }
 
-    cout << "Сумма " << a << " и " << b << " = " << Plus(a, b) << endl;
-    cout << "Разность " << a << " и " << b << " = " << Minus(a, b) << endl;
+    cout << "Сумма " << a << " + " << b << " = " << Plus(a, b) << endl;
+    cout << "Разность " << a << " + " << b << " = " << Minus(a, b) << endl;
+	cout << "Умножение " << a << " * " << b << " = " << a * b << endl;
 
     return 0;
 }
